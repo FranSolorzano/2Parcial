8c3969b9888ae1c2807d1b3d4a140d5d7c56a334
FranSolorzano <87993086+FranSolorzano@users.noreply.github.com>
2021-11-09 00:16:51 -0600

    SegundoParcial

---
+++ SegundoParcial/SegundoParcial/Controllers/CajeroController.cs
@@ -0,0 +1,55 @@
+﻿using SegundoParcial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SegundoParcial.Controllers
+{
+    public class CajeroController : Controller
+    {
+        
+        
+     [HttpGet]
+
+      public ActionResult Index1()
+      {
+         return View();
+      }
+
+      [HttpPost]
+
+      public ActionResult Index1(Cajero C)
+      {
+         if (C.cantidad % 5 == 0)
+         {
+            return Redirect("VistaDatos");
+         }
+         else
+         {
+            return Redirect("Error");
+
+         }
+
+      }
+
+      [HttpGet]
+
+      public ActionResult Error()
+      {
+          return View();
+
+      }
+
+      [HttpGet]
+
+      public ActionResult VistaDatos()
+      {
+          return View();
+
+      }
+
+    }
+}
+
